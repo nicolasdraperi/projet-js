358d9ca7b477f35a85ff97544f459c0d347271dc
nicolasdraperi <113093384+nicolasdraperi@users.noreply.github.com>
2023-11-08 16:47:56 +0100

    Accueil updated

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const themeButton = document.getElementById("theme");
-
+//bouton theme sombre
 themeButton.addEventListener("click", () => {
     document.body.classList.toggle("dark-mode");   
 });
