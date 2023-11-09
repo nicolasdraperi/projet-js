1d7c562c44ce2fa114368db119101cfe0670cf3b
nicolasdraperi <113093384+nicolasdraperi@users.noreply.github.com>
2023-11-09 10:29:47 +0100

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -16,5 +16,21 @@ $usrid = $_POST["id"];;
 $usrmdp=$_POST["password"];
 
 
+$requete = $conn->prepare("SELECT * FROM `user` WHERE id = :usrid");
+$requete->bindParam(':usrid', $usrid);
+$requete->execute();
 
+if ($row = $requete->fetch(PDO::FETCH_ASSOC)) {
+
+    if (password_verify($usrmdp, $row[''])) {
+        echo "Connexion réussie. Bienvenue, " . $row[''];
+
+    } else {
+        echo "Mot de passe incorrect";
+    }
+} else {
+    echo "Identifiant incorrect";
+}
+
+$requete->closeCursor();
 ?>
