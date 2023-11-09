526e73bc48999ec5f706aa932598b55ab8204e08
nicolasdraperi <113093384+nicolasdraperi@users.noreply.github.com>
2023-11-09 10:15:47 +0100

    page login

--- connexion.php
+++ connexion.php
@@ -1,15 +1,20 @@
 <?php
 
-$host = "localhost"; // le chemin vers le serveur
+$host = "localhost"; 
 $port = "3306";
-$dbname = "bob"; // le nom de votre BDD
-$username = "root"; // nom d'utilisateur pour se connecter
-$password = ""; // mot de passe pour se connecter
+$dbname = "bob"; 
+$username = "root"; 
+$password = ""; // 
 try {
     $conn = new PDO('mysql:host=' . $host . ';port=' . $port . ';dbname=' . $dbname, $username, $password);
     echo "connexion reussi";
 } catch (PDOException $except) {
     echo "Echec de la connexion : " . $except->getMessage();
 }
+
+$usrid = $_POST["id"];;
+$usrmdp=$_POST["password"];
+
+
 
 ?>
